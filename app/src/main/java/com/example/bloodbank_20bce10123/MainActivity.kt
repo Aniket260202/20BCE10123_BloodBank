404b3a0bd82ace981b4c0f2ac056a2902e8ffe17
Aniket Kumar Jha <aniket260202@gmail.com>
2023-06-28 18:10:34 +0530

    FIrst Commit

--- app/src/main/java/com/example/bloodbank_20bce10123/MainActivity.kt
+++ app/src/main/java/com/example/bloodbank_20bce10123/MainActivity.kt
@@ -16,10 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
